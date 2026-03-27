939e9722d98393cfaac05f696f00a8e5e39c7d4d
Jesus Alejandro Garcia <garciacode96@gmail.com>
2026-03-26 22:27:55 -0600

    Arreglos y y arreglos asociativos

--- strings.php
+++ strings.php
@@ -16,4 +16,30 @@
   SQL;
 
   echo "$cadena_multilinea\n\n";
-  echo $detalle_jugador;
+  echo $detalle_jugador . "\n\n";
+
+  # Manejo de arrays - solo se pueden imprimir con print_r o var_dumb()
+  $languages = ['English', 'German', 'Spanish', 'Chinese'];
+  print_r($languages);
+
+  # Manera de hacer push a un array
+  $languages[] = 'French';
+  print_r($languages);
+
+  echo "\nTercer idioma: $languages[3]\n";
+
+  # Arrays asociativos
+  $languages_formated = [
+    'EN' => 'English',
+    'GE' => 'German',
+    'CN' => 'Chinese',
+    'SP' => 'Spanish'
+  ];
+  print_r($languages_formated);
+
+  $languages_formated['JP'] = 'Japanese';
+  print_r($languages_formated);
+
+  echo "\nÚltimo idioma: $languages_formated[JP]\n";
+  echo $languages_formated['GE'];
+
